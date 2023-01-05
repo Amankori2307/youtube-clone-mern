dab9249b88523f25abe3374fa71d3acb133a52a2
Your Name <you@example.com>
2023-01-05 17:41:31 +0530

    Established database connection

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
 dotenv.config();
 const app = express();
 
+// Connecting to database
+const uri = process.env.MONGO_URI;
+mongoose.set({
+    strictQuery: false,
+});
+mongoose.connect(uri, { useNewUrlParser: true }, () => {
+    console.log('Successfly connection to database...');
+});
 
-const port = process.env.PORT
+app.use(express.json());
+
+// Creating HTTP Server
+const port = process.env.PORT;
 app.listen(port, () => {
-    console.log(`Listenting on http://127.0.0.1:${port}`)
-})
+    console.log(`Listenting on http://127.0.0.1:${port} ...`);
+});
